56cadfbddfc3bf0ebfc92aad71084d00d13eff12
kiran <kiranjha.spearmint@gmail.com>
2023-05-01 13:16:45 +0530

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,12 +6,12 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     fantom: {
-      url: `https://ftm.getblock.io/7176162d-8e17-43dc-a78a-2837fcc423d3/testnet/`,
-      accounts:[`0x4ba35480719fa68c04f812b60a58c78c05674d9ec35f87795cae3c32b55e74fc`],
+      url: `${ process.env.FANTOM_PROVIDER }`,
+      accounts:[`${process.env.PRIVATE_KEY}`],
     },
   },
   etherscan: {
-    apiKey: `T1CP27RPHNJ79V379B1W2PEF3UBJDH8AG2`,
+    apiKey: `${process.env.FANTOMSCAN_APIKEY}`,
   },
   settings: {
     optimizer: {
